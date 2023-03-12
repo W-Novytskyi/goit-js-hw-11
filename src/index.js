d618e230a24bfe9d8ec3d1ed1312459c6c38a79f
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-03-13 00:17:28 +0200

    ???

--- src/index.js
+++ src/index.js
@@ -1,1 +1,79 @@
-dfdd
+
+import { Notify } from 'notiflix';
+import NewsApiService from './news-service';
+import './css/styles.css';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    loadMoreBtn: document.querySelector('.load-more'),
+    gallery: document.querySelector('.gallery')
+}
+
+const newsApiService = new NewsApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  newsApiService.query = e.currentTarget.searchQuery.value.trim();
+    newsApiService.resetPage();
+    
+  newsApiService.fetchArticles()
+      .then(({ data }) => {
+      if (data.total === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      } else {
+        renderImages(data.hits);
+      }
+    })
+    .catch(error => {
+      Notify.failure('Something went wrong. Please try again later.');
+      console.error(error);
+    });
+}
+
+
+function onLoadMore() {
+    newsApiService.fetchArticles()
+        .then(({ data }) => {
+            renderImages(data.hits);
+        })
+        .catch(error => {
+            Notify.failure('Something went wrong. Please try again later.');
+            console.error(error);
+        });
+}
+
+
+function renderImages(hits) {
+    const markup = hits
+        .map(hit => {
+        return `
+            <div class="photo-card">
+                <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+                <div class="stats">
+                    <p class="stats-item">
+                        <i class="material-icons">thumb_up</i>
+                        ${hit.likes}
+                    </p>
+                    <p class="stats-item">
+                        <i class="material-icons">visibility</i>
+                        ${hit.views}
+                    </p>
+                    <p class="stats-item">
+                        <i class="material-icons">comment</i>
+                        ${hit.comments}
+                    </p>
+                    <p class="stats-item">
+                        <i class="material-icons">cloud_download</i>
+                        ${hit.downloads}
+                    </p>
+                </div>
+            </div>
+        `;
+    }).join('');
+
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
---
+++ src/news-service.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const API_KEY = '34344088-cfac681c64979560ee45228c3';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class NewsApiService {
+    constructor() {
+        this.searchTerm = ' ';
+        this.page = 1;
+    }
+
+    fetchArticles() {
+        const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+
+        return axios.get(url)
+            .then(response => response.data)
+            .then(data => {
+                this.incrementPage();
+
+                return data.hits;
+            });
+    }
+        
+    incrementPage() {
+        this.page += 1;
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchTerm;
+    }
+
+    set query(newQuary) {
+        this.searchTerm = newQuary;
+    }
+
+}
